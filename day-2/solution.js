aac5e7bccf57fdeeae09aed277c9ac8729e7e3d2
Adam Risberg <myniztan@hotmail.com>
2019-12-02 02:27:20 -0600

    Finish day 2 solution

---
+++ day-2/solution.js
@@ -0,0 +1,20 @@
+const input = require("./input");
+const Computer = require("./computer");
+const utils = require("./utils");
+
+const program = [...input];
+program[1] = 12;
+program[2] = 2;
+
+const computer = new Computer();
+
+computer.setProgram(program);
+computer.compute();
+console.log(`Part 1: ${computer.getOutput()}`);
+
+computer.resetProgram();
+
+const result = computer.findNounAndVerb(19690720);
+const noun = utils.padNumber(result[0], 2);
+const verb = utils.padNumber(result[1], 2);
+console.log(`Part 2: ${noun + verb}`);
