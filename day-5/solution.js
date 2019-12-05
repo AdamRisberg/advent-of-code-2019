b8d61819f2c2ba837522fe3abe724869f1a4922b
Adam Risberg <myniztan@hotmail.com>
2019-12-05 16:11:02 -0600

    Clean up day 5 solution

--- day-5/solution.js
+++ day-5/solution.js
@@ -1,9 +1,14 @@
-const input = require("./input");
+const program = require("./input");
 const Computer = require("./computer");
 
-const program = [...input];
-
 const computer = new Computer();
-computer.loadProgram(program);
 
-computer.run();
+computer.loadProgram(program);
+computer.queueInputs(1);
+const outputs1 = computer.run(true);
+console.log(`Part 1: ${outputs1[outputs1.length - 1]}`);
+
+computer.resetProgram();
+computer.queueInputs(5);
+const outputs2 = computer.run(true);
+console.log(`Part 2: ${outputs2[outputs2.length - 1]}`);
