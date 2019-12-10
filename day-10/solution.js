1305883d90c8d24e688c6ba27afd4ae15ba00353
Adam Risberg <myniztan@hotmail.com>
2019-12-10 04:00:17 -0600

    Finish day 10 solution

--- day-10/solution.js
+++ day-10/solution.js
@@ -1,1 +1,23 @@
-const input = require("./input");
+const inputs = require("./inputs");
+const stationFinder = require("./station-finder");
+const {
+  findBestMonitoringLocation,
+  getTwoHundrethAsteroidDestroyed
+} = stationFinder;
+
+const monitoringStation = findBestMonitoringLocation(inputs.main);
+
+console.log(`Part 1: ${monitoringStation.count}`);
+
+const asteroid = getTwoHundrethAsteroidDestroyed(
+  inputs.main,
+  monitoringStation.location
+);
+
+if (asteroid) {
+  const { location } = asteroid;
+  const part2 = location.x * 100 + location.y;
+  console.log(`Part 2: ${part2}`);
+} else {
+  console.log("Part 2: No 200th asteroid found!");
+}
