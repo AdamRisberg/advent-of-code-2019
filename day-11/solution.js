57ddbd050904f2d175676a570082e03055b545f2
Adam Risberg <myniztan@hotmail.com>
2019-12-11 02:12:01 -0600

    Finish day 11 solution

--- day-11/solution.js
+++ day-11/solution.js
@@ -1,1 +1,19 @@
 const input = require("./input");
+const paintingRobot = require("./painting-robot");
+const imageProcessor = require("../day-8/image-processor");
+const {
+  createPaintingInfo,
+  getPaintedSquaresCount,
+  createPaintingGrid
+} = paintingRobot;
+
+const paintingInfoPart1 = createPaintingInfo(input);
+const part1 = getPaintedSquaresCount(paintingInfoPart1);
+
+console.log(`Part 1: ${part1}`);
+
+const paintingInfoPart2 = createPaintingInfo(input, 1);
+const paintingGrid = createPaintingGrid(paintingInfoPart2);
+imageProcessor.createImage(paintingGrid, 10, "Part2");
+
+console.log("Part 2: See Part2.jpg");
