d8ef6051d790e20a829ff50bb14ee1522061340c
Adam Risberg <myniztan@hotmail.com>
2019-12-12 03:10:24 -0600

    Add puzzle and test inputs

--- day-12/input.js
+++ day-12/input.js
@@ -1,9 +1,22 @@
-const path = require("path");
-const fs = require("fs");
+const Moon = require("./moon");
 
-const input = fs.readFileSync(
-  path.resolve(__dirname, "raw-input.txt"),
-  "UTF-8"
-);
-
-module.exports = input;
+module.exports = {
+  getMainInput: () => [
+    new Moon(16, -11, 2),
+    new Moon(0, -4, 7),
+    new Moon(6, 4, -10),
+    new Moon(-3, -2, -4)
+  ],
+  getTestInput1: () => [
+    new Moon(-1, 0, 2),
+    new Moon(2, -10, -7),
+    new Moon(4, -8, 8),
+    new Moon(3, 5, -1)
+  ],
+  getTestInput2: () => [
+    new Moon(-8, -10, 0),
+    new Moon(5, 5, 10),
+    new Moon(2, -7, 3),
+    new Moon(9, -8, -3)
+  ]
+};
