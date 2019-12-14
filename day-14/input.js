e45987fd70a09c9bc4fb9ea6de7c9e1056ac72a2
Adam Risberg <myniztan@hotmail.com>
2019-12-14 03:09:02 -0600

    Add puzzle and test inputs

--- day-14/input.js
+++ day-14/input.js
@@ -1,9 +1,38 @@
 const path = require("path");
 const fs = require("fs");
 
-const input = fs.readFileSync(
-  path.resolve(__dirname, "raw-input.txt"),
-  "UTF-8"
-);
+const getInput = inputFilePath => {
+  return fs
+    .readFileSync(path.resolve(__dirname, inputFilePath), "UTF-8")
+    .split("\r\n")
+    .map(line => {
+      let [ingredients, result] = line.split("=>");
 
-module.exports = input;
+      const ingredientsArray = ingredients
+        .trim()
+        .split(",")
+        .map(i => i.trim().split(" "));
+      const resultArray = result.trim().split(" ");
+
+      return {
+        result: resultArray,
+        ingredients: ingredientsArray
+      };
+    })
+    .reduce((acc, next) => {
+      const parentKey = next.result[1];
+
+      acc[parentKey] = {
+        amount: Number(next.result[0]),
+        store: 0
+      };
+
+      for (let i = 0; i < next.ingredients.length; i++) {
+        acc[parentKey][next.ingredients[i][1]] = Number(next.ingredients[i][0]);
+      }
+
+      return acc;
+    }, {});
+};
+
+module.exports = getInput;
