54944902d78d41287e0c6752ebff008f6ecb5ee9
Adam Risberg <myniztan@hotmail.com>
2019-12-15 18:10:36 -0600

    Add inputs for day 15

--- day-15/input.js
+++ day-15/input.js
@@ -1,9 +1,27 @@
 const path = require("path");
 const fs = require("fs");
 
-const input = fs.readFileSync(
-  path.resolve(__dirname, "raw-input.txt"),
-  "UTF-8"
-);
+const input = fs
+  .readFileSync(path.resolve(__dirname, "raw-input.txt"), "UTF-8")
+  .split(",")
+  .map(str => Number(str));
 
 module.exports = input;
+
+// 1 north
+// 2 south
+// 3 west
+// 4 east
+
+// 0: The repair droid hit a wall. Its position has not changed.
+// 1: The repair droid has moved one step in the requested direction.
+// 2: The repair droid has moved one step in the requested direction;
+//    its new position is the location of the oxygen system.
+
+// directions.east,
+// directions.east,
+// directions.north,
+// directions.north,
+// directions.east,
+// directions.north,
+// directions.east
