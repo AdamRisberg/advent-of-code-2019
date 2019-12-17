1988dbfadd7c0d9336a1e44489239327382706db
Adam Risberg <myniztan@hotmail.com>
2019-12-16 18:31:33 -0600

    Finish day 16 solution

--- day-16/solution.js
+++ day-16/solution.js
@@ -1,1 +1,9 @@
 const input = require("./input");
+const signalProcessor = require("./signal-processor");
+const { processSignal, processSignalWithRepeat } = signalProcessor;
+
+const part1 = processSignal([...input], 100, [0, 1, 0, -1]);
+const part2 = processSignalWithRepeat([...input], 100, 10000);
+
+console.log(`Part 1: ${part1}`);
+console.log(`Part 2: ${part2}`);
