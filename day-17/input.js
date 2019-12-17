a0c75759a235bb6a7beb7f4a47375cf9b10692b6
Adam Risberg <myniztan@hotmail.com>
2019-12-17 04:18:44 -0600

    Add puzzle inputs

--- day-17/input.js
+++ day-17/input.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const fs = require("fs");
 
-const input = fs.readFileSync(
-  path.resolve(__dirname, "raw-input.txt"),
-  "UTF-8"
-);
+const input = fs
+  .readFileSync(path.resolve(__dirname, "raw-input.txt"), "UTF-8")
+  .split(",")
+  .map(Number);
 
 module.exports = input;
