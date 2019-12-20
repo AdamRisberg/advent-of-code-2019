fc544db81d97dca6afd0c969089503b30325aee5
Adam Risberg <myniztan@hotmail.com>
2019-12-19 18:58:56 -0600

    Finish day 18 solution

--- day-18/solution.js
+++ day-18/solution.js
@@ -1,1 +1,10 @@
-const input = require("./input");
+const inputs = require("./input");
+const { inputPart1, inputPart2 } = inputs;
+const maze = require("./maze");
+const { collectAllKeys } = maze;
+
+const part1 = collectAllKeys(inputPart1);
+console.log(`Part 1: ${part1}`);
+
+const part2 = collectAllKeys(inputPart2);
+console.log(`Part 2: ${part2}`);
