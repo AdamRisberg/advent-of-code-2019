0be1187d146e67bbd34ead518e46cf8a96713454
Adam Risberg <myniztan@hotmail.com>
2019-12-21 14:26:12 -0600

    Finish day 20 solution

--- day-20/solution.js
+++ day-20/solution.js
@@ -1,1 +1,9 @@
-const input = require("./input");
+const inputs = require("./input");
+const { input } = inputs;
+const solveMaze = require("./maze");
+
+const part1 = solveMaze(input);
+const part2 = solveMaze(input, 25);
+
+console.log(`Part 1: ${part1}`);
+console.log(`Part 2: ${part2}`);
