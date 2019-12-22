d346ef82b03d94c72724864fd5616c58f51c9217
Adam Risberg <myniztan@hotmail.com>
2019-12-22 14:58:00 -0600

    Finish day 22 solution

--- day-22/solution.js
+++ day-22/solution.js
@@ -1,1 +1,15 @@
-const input = require("./input");
+const inputs = require("./input");
+const { input } = inputs;
+const shuffle = require("./shuffle");
+const { shuffleAndFindCardIndex, simulateCardShuffle } = shuffle;
+
+const part1 = shuffleAndFindCardIndex(testInput, 10007, 2019);
+console.log(`Part 1: ${part1}`);
+
+const part2 = simulateCardShuffle(
+  input,
+  119315717514047,
+  101741582076661,
+  2020
+);
+console.log(`Part 2: ${part2}`);
