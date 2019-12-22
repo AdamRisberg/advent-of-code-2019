b5b2f0dd66129bcab798be8870abecd208b24223
Adam Risberg <myniztan@hotmail.com>
2019-12-22 14:56:18 -0600

    Add day 22 puzzle inputs

--- day-22/input.js
+++ day-22/input.js
@@ -1,9 +1,12 @@
 const path = require("path");
 const fs = require("fs");
 
-const input = fs.readFileSync(
-  path.resolve(__dirname, "raw-input.txt"),
-  "UTF-8"
-);
+const input = fs
+  .readFileSync(path.resolve(__dirname, "raw-input.txt"), "UTF-8")
+  .split("\r\n");
 
-module.exports = input;
+const testInput = fs
+  .readFileSync(path.resolve(__dirname, "raw-test-input.txt"), "UTF-8")
+  .split("\r\n");
+
+module.exports = { input, testInput };
